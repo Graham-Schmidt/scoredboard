b65ff019dd18c9aa73d652c171263f30fc881d07
HamBoneGS <graham.n.schmidt@gmail.com>
2026-02-09 16:05:59 -0600

    save

--- scoredboard/scoredboard/models.py
+++ scoredboard/scoredboard/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name: str
+    team_a_name: str
+    team_b_name: str
+    team_a_score: int
+    team_b_score: int
+    game_cdoe: str
+    expiration_datetime: datetime
+
+    
--- scoredboard/scoredboard/views.py
+++ scoredboard/scoredboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def display_board(request):
+    template = loader.get_template("display.html")
+    return HttpResponse(template.render({}, request))
+
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render({}, request))
